a10da9173270f4e4c715aa58fd6f8aae8c3641ad
Kurniawan Pratama <kurniawanpputra@gmail.com>
2018-10-19 12:56:23 +0000

    Review Backend Validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,9 @@
 class Review < ApplicationRecord
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+            uniqueness: {case_sensitive: false},
+            length: {maximum: 100},
+            format: {with: VALID_EMAIL_REGEX}
+  validates :description, presence: true
 end
