8cbba68e859dbe4ea470582256cd250fd0b8c1b6
Kurniawan Pratama <kurniawanpputra@gmail.com>
2018-10-19 03:08:54 +0000

    Create Review Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181014232950) do
+ActiveRecord::Schema.define(version: 20181019030517) do
 
   create_table "foods", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(version: 20181014232950) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "img"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
