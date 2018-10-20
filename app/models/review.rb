91a8e70ea8340bb1cc9630defe86a756cdf435b0
Kurniawan Pratama <kurniawanpputra@gmail.com>
2018-10-20 08:50:37 +0000

    Update the Views

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,9 +1,9 @@
 class Review < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: {maximum: 25}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true,
             uniqueness: {case_sensitive: false},
             length: {maximum: 100},
             format: {with: VALID_EMAIL_REGEX}
-  validates :description, presence: true
+  validates :description, presence: true, length: {maximum: 200}
 end
