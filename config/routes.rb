a77c2c88df1dbef8c96fa599f941d6c703d1a6e6
Kurniawan Pratama <kurniawanpputra@gmail.com>
2018-12-09 14:37:03 +0000

    Create About Page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :reviews
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
-  # get 'about', to: 'pages#about'
+  get 'about', to: 'pages#about'
   resources :foods, except: [:destroy, :create, :new, :edit, :update]
 end
