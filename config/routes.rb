652985af25590ae76b1b1c65edf289f0a9459c51
Kurniawan Pratama <kurniawanpputra@gmail.com>
2018-12-10 00:09:27 +0000

    Route Update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :reviews, :only => [:index, :show, :destroy, :create, :new]
+  resources :reviews
+  get 'edit_review', to: 'reviews#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
